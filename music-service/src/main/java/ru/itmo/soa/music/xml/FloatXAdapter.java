f0b1d7ee6f31e5280cb78075d2171f513526429f
Romariok <ronya-ko@yandex.ru>
2025-10-03 10:51:21 +0300

    add digits count after dot check in float adapter

--- music-service/src/main/java/ru/itmo/soa/music/xml/FloatXAdapter.java
+++ music-service/src/main/java/ru/itmo/soa/music/xml/FloatXAdapter.java
@@ -5,9 +5,17 @@ import jakarta.xml.bind.annotation.adapters.XmlAdapter;
 public class FloatXAdapter extends XmlAdapter<String, Double> {
     @Override
     public Double unmarshal(String v) {
-        if (v == null) return null;
+        if (v == null)
+            return null;
         if (!v.matches("^-?\\d+(\\.\\d+)?$")) {
             throw new IllegalArgumentException("Field 'coordinates.x' must be number (float)");
+        }
+        int dotIndex = v.indexOf('.');
+        if (dotIndex >= 0) {
+            int fractionalDigits = v.length() - dotIndex - 1;
+            if (fractionalDigits > 10) {
+                throw new IllegalArgumentException("Field 'coordinates.x' must have at most 5 digits after decimal");
+            }
         }
         try {
             return Double.parseDouble(v);
@@ -21,5 +29,3 @@ public class FloatXAdapter extends XmlAdapter<String, Double> {
         return v == null ? null : v.toString();
     }
 }
-
-
